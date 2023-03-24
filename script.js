f72e1caf63d7de0785c248710bb27ea44c820d32
Arya Setiawan <aryasetiawan.st@gmail.com>
2023-03-24 13:21:49 +0700

    Menambahkan Character Goat

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-const canvas = document.getElementById("canvas1");
+const cat = document.getElementById("canvas1");
+const goat = document.getElementById("canvas2");
+const dog = document.getElementById("canvas3");
 const arrowL = document.getElementById("left-arrow");
 const arrowR = document.getElementById("right-arrow");
-characters = ["Sources/Dog.png", "Sources/Cat.jpg"];
+characters = ["Sources/Cat.jpg", "Sources/Goat.png", "Sources/Dog.png"];
 
-const ctx = canvas.getContext("2d");
+const ctx1 = cat.getContext("2d");
+const ctx2 = goat.getContext("2d");
+const ctx3 = dog.getContext("2d");
 const playerImg = new Image();
-playerImg.src = characters[0];
+playerImg.src = characters[1];
 
-const CANVAS_WIDTH = (canvas.width = 110);
-const CANVAS_HEIGHT = (canvas.height = 110);
+const CAT_WIDTH = (cat.width = 150);
+const CAT_HEIGHT = (cat.height = 150);
+const GOAT_WIDTH = (goat.width = 150);
+const GOAT_HEIGHT = (goat.height = 150);
+const DOG_WIDTH = (dog.width = 150);
+const DOG_HEIGHT = (dog.height = 150);
 
 const spriteWidth = 96; //kucing = 141
 const spriteheight = 96; //kucing = 141
 let frameX = 0;
-let frameY = 7;
+let frameY = 4;
 let gameFrame = 0;
 let character;
 
 function animate() {
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.drawImage(playerImg, frameX * spriteWidth, frameY * spriteheight, spriteWidth, spriteheight, 20, 10, spriteWidth, spriteheight);
-  if (gameFrame % 50 == 0) {
-    if (frameX < 1) frameX++;
+  ctx2.clearRect(0, 0, GOAT_WIDTH, GOAT_HEIGHT);
+  ctx2.drawImage(playerImg, frameX * spriteWidth, frameY * spriteheight, spriteWidth, spriteheight, 25, 25, spriteWidth, spriteheight);
+  if (gameFrame % 35 == 0) {
+    if (frameX < 3) frameX++;
     else frameX = 0;
   }
 
